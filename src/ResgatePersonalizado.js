f29458f183d011447786aca0bab117427a9c96f3
Jean Henrique <jean.jaques@ufms.br>
2022-01-19 12:20:47 -0400

    add modal de erro

--- src/ResgatePersonalizado.js
+++ src/ResgatePersonalizado.js
@@ -1,6 +1,9 @@
 import { useState, useLayoutEffect } from 'react';
 import { View, Text, FlatList, StyleSheet, SafeAreaView, Button } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Modal } from 'react-native-paper';
+import CurrencyInput from 'react-native-currency-input';
+
+
 
 
 //componentes ou libs
@@ -9,6 +12,11 @@ import { formatNumber } from '../lib/format';
 export default function ResgatePersonalizado({ navigation, route }) {
     const [listaAcoes, setListaAcoes] = useState([]);
     const [valorResgate, setValorResgate] = useState(0);
+
+    //configurações dos modais
+    const [visibilidadeModalErro, setVisibilidadeModalErro] = useState(false);
+    const showModalErro = () => setVisibilidadeModalErro(true);
+    const hideModalErro = () => setVisibilidadeModalErro(false);
 
     useLayoutEffect(() => {
         setListaAcoes(route.params.listaAcoes)
@@ -19,7 +27,6 @@ export default function ResgatePersonalizado({ navigation, route }) {
         let valorConvertido = formatNumber(valor)
         return valorConvertido
     }
-
 
     return (
         <View style={styles.container}>
@@ -48,16 +55,25 @@ export default function ResgatePersonalizado({ navigation, route }) {
                             <Text style={styles.tituloConteudo}>Saldo Total Disponível</Text>
                             <Text style={styles.informacaoConteudo}>{converteParaMoeda(route.params.saldoTotal*(item.percentual/100))}</Text>
                         </View>
+                        <View style={styles.conteudo}>
+                            <Text style={styles.informacaoConteudo}>Valor a resgatar</Text>
+                        </View>
                         <View>
-                            <TextInput style={styles.input} mode="focused" color='white' label="Valor a resgatar" onChangeText={setValorResgate} style={styles.input}></TextInput>
+                            <CurrencyInput error={parseFloat(valorResgate) > parseFloat(route.params.saldoTotal*(item.percentual/100)) } style={styles.input} mode="focused" color='white' prefix="R$" label="Valor a resgatar" value={valorResgate} onChangeValue={valorResgate => setValorResgate(valorResgate)} style={styles.input}   
+                            ></CurrencyInput>
                         </View>
                     </View>
                 )}
                 />
             </View>
             <View>
-                <Button title='Resgatar'>Resgatar</Button>
+                <Button title='Resgatar' onPress={showModalErro}>Resgatar</Button>
             </View>
+            <Modal visible={visibilidadeModalErro} onDismiss={hideModalErro} contentContainerStyle={styles.containerModal}>
+                    <Text style={styles.tituloModal}>Dados Invalidos</Text>
+                    <Text style={styles.conteudoModal}>Você preencheu um ou mais campos com valores fora dos permitidos</Text>
+                    <Button title='Corrigir' onPress={hideModalErro}></Button>
+                </Modal>
         </View>
     )
 }
@@ -99,6 +115,31 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
     input:{
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        color: 'black',
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginHorizontal: 15
+    },
+    containerModal:{
+        backgroundColor: 'white', 
+        padding: 20, 
+        marginHorizontal: 15
+    },
+    tituloModal:{
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginHorizontal: 15,
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        padding: 5,
+    },
+    conteudoModal:{
+        color: 'grey',
+        fontWeight: 'bold',
+        fontSize: 14,
+        marginHorizontal: 15,
+        textAlign: 'center',
+        padding: 15,
     }
 })
