0fa9b005981e8c4876d872651f457ccb051fc4c1
Jean Jaques <45767643+jeanhjaques@users.noreply.github.com>
2022-01-26 11:54:14 -0400

    Corrigido problema em impedir que investimentos em carência avancem.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import { useLayoutEffect, useState } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Button } from 'react-native';
+import { Modal } from 'react-native-paper'
 
 //componentes ou libs
 import CardListaInvestimentos from './components/CardListaInvestimentos'
@@ -7,7 +8,11 @@ import CardListaInvestimentos from './components/CardListaInvestimentos'
 
 export default function HomeScreen({ navigation }) {
     const [listaInvestimentos, setListaInvestimentos] = useState([])
-    const [loading, setLoading] = useState(true);
+    const [visibilidadeModalCarencia, setVisibilidadeModalCarencia] = useState(false);
+    const showModalCarencia = () => setVisibilidadeModalCarencia(true);
+    const hideModalCarencia = () => setVisibilidadeModalCarencia(false);
+
+
 
     useLayoutEffect(() => {
         fetch('https://run.mocky.io/v3/ca4ec77d-b941-4477-8a7f-95d4daf7a653')
@@ -31,12 +36,16 @@ export default function HomeScreen({ navigation }) {
                     data={listaInvestimentos.listaInvestimentos}
                     keyExtractor={({ nome }, index) => nome}
                     renderItem={({ item }) => (
-                        <TouchableOpacity onPress={() => navigation.navigate('Resgate Personalizado', { listaAcoes: item.acoes, nome: item.nome, saldoTotal: item.saldoTotal })}>
+                        <TouchableOpacity onPress={item.indicadorCarencia == "N" ? () => navigation.navigate('Resgate Personalizado', { listaAcoes: item.acoes, nome: item.nome, saldoTotal: item.saldoTotal }) : () => showModalCarencia()}>
                             <CardListaInvestimentos nome={item.nome} subtitulo={item.objetivo} valor={item.saldoTotal} />
                         </TouchableOpacity>
                     )}
                 />
             </View>
+            <Modal visible={visibilidadeModalCarencia} onDismiss={hideModalCarencia} contentContainerStyle={styles.containerModal}>
+                    <Text style={styles.tituloModal}>Esse fundo de investimento está em carência</Text>
+                    <Button color='red' title='Voltar' onPress={hideModalCarencia}></Button>
+            </Modal>
         </View>
     );
 }
@@ -55,6 +64,18 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         textTransform: 'uppercase',
         color: 'grey'
-    }
+    },
+    containerModal: {
+        backgroundColor: 'white',
+        padding: 10,
+        margin: 15
+    },
+    tituloModal: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginHorizontal: 15,
+        textTransform: 'uppercase',
+        textAlign: 'center',
+        padding: 5,
+    },
 })
-
